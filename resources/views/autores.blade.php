1ea4c30328c074243b990a67882363226f6be18b
Pedro Wang <pre7758@hotmail.com>
2025-07-29 13:07:02 +0100

    Adicionado uma tabela na pagina de livros

--- resources/views/autores.blade.php
+++ resources/views/autores.blade.php
@@ -3,15 +3,7 @@
         Autor Page
     </x-slot:heading>
 
-    {{-- <div class = "space-y-6>
-        @foreach ($autores as $autor)
-            <div class="border border-gray-200 rounded-lg p-4">
-                <h2 class="text-xl font-bold">{{ $autor->name }}</h2>
-                <img src="{{ $autor->foto }}" alt="{{ $autor->name }}" class="w-12 h-12 object-cover rounded-full">
-            </div>
-        @endforeach
-    </div> --}}
-
+    
     <div class="space-y-6 flex flex-col items-center">
     @foreach ($autores as $autor)
         <div class="rounded-lg p-4 w-64 text-center">
--- resources/views/livros.blade.php
+++ resources/views/livros.blade.php
@@ -2,5 +2,34 @@
     <x-slot:heading>
         Livro Page
     </x-slot:heading>
-    <h1 class="text-3xl font-bold text-center mt-6">Hello from Livro Page</h1>
+    <h1 class="text-2xl font-semibold text-center mt-6">Lista de Livros</h1>
+
+    <div class="mt-6 px-4">
+        <table class="w-full border border-gray-300">
+            <thead>
+                <tr class="bg-gray-100">
+                    <th class="p-2 border">ISBN</th>
+                    <th class="p-2 border">Nome</th>
+                    <th class="p-2 border">Editora</th>
+                    <th class="p-2 border">Bibliografia</th>
+                    <th class="p-2 border">Imagem</th>
+                    <th class="p-2 border">Preço</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($livros as $livro)
+                    <tr>
+                        <td class="p-2 border">{{ $livro->isbn }}</td>
+                        <td class="p-2 border">{{ $livro->name }}</td>
+                        <td class="p-2 border">{{ $livro->editor->name }}</td>
+                        <td class="p-2 border">{{ $livro->bibliography }}</td>
+                        <td class="p-2 border text-center">
+                            <img src="{{ $livro->image }}" alt="{{ $livro->name }}" class="w-12 h-12 object-cover mx-auto rounded-full">
+                        </td>
+                        <td class="p-2 border text-right">€ {{ $livro->price }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-app-layout>
